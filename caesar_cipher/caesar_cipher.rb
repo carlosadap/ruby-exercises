f76f7c9a370b0c465a5ba8b7e09cfe108df745b6
Carlos Pinheiro <carlosadap@gmail.com>
2021-03-05 12:37:08 +0100

    Refactor the code

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,22 +1,23 @@
 def caesar_cipher(string, code)
+  cipher_array = string.split("").map { |char| cipher_char(char, code) }
+  cipher_array.join("")
+end
+
+def cipher_char(char, code)
   lowercase_alphabet = ('a'..'z').to_a
   uppercase_alphabet = ('A'..'Z').to_a
 
-  cipher_array =string.split("").map do |char|
-    if lowercase_alphabet.include?(char)
-      old_idx = lowercase_alphabet.index(char)
-      new_idx = ( old_idx + code ) % lowercase_alphabet.length
-      lowercase_alphabet[new_idx]
-    elsif uppercase_alphabet.include?(char)
-      old_idx = uppercase_alphabet.index(char)
-      new_idx = ( old_idx + code ) % uppercase_alphabet.length
-      uppercase_alphabet[new_idx]
-    else
-      char
-    end
+  if lowercase_alphabet.include?(char)
+    old_idx = lowercase_alphabet.index(char)
+    new_idx = ( old_idx + code ) % lowercase_alphabet.length
+    lowercase_alphabet[new_idx]
+  elsif uppercase_alphabet.include?(char)
+    old_idx = uppercase_alphabet.index(char)
+    new_idx = ( old_idx + code ) % uppercase_alphabet.length
+    uppercase_alphabet[new_idx]
+  else
+    char
   end
-
-  cipher_array.join("")
 end
 
 puts caesar_cipher("What a string!", 5)
